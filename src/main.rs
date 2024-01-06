e2db19bfd83891b355a103b55065be73407ee34f
Creativious <timothyu120@gmail.com>
2024-01-06 04:30:11 -0500

    back to the old one I guess

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use shared_memory::shared_memory::SharedMemory;
 
 fn main() {
-    let mut shm = SharedMemory::create("test", 5).unwrap();
+    let mut shm = SharedMemory::create("test", 1000).unwrap();
     shm.write_string("Hello World!");
 
-    let mut shm_other = SharedMemory::open("test", 5).unwrap();
+    let shm_other = SharedMemory::open("test", 1000).unwrap();
     // convert the data to a string and then print it
     println!("{}", shm_other.read_string());
 
