00c4bb62b2a9f60d7e6c554f0ecad05693dfc9f4
Creativious <timothyu120@gmail.com>
2024-01-06 04:17:50 -0500

    fixed the usage as you're supposed to use open not create

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     let mut shm = SharedMemory::create("test", 1024).unwrap();
     shm.write_string("Hello World!");
 
-    let mut shm_other = SharedMemory::create("test", 1024).unwrap();
+    let mut shm_other = SharedMemory::open("test", 1024).unwrap();
     // convert the data to a string and then print it
     println!("{}", shm_other.read_string());
 
